3d3d2bf7575c26c826ed7d92931bc2ee5926545f
BennaceurHichem <gh_bennaceur@esi.dz>
2020-07-28 11:55:31 +0100

    movie detail page finished

--- client/src/components/views/MovieDetailPage/MovieDetailPage.js
+++ client/src/components/views/MovieDetailPage/MovieDetailPage.js
@@ -1,7 +1,8 @@
 import React,{useEffect,useState} from 'react'
 import {API_KEY,API_URL,} from '../../Config'
-
-
+import { Descriptions, Badge } from 'antd';
+import {IMAGE_URL,MAIN_IMAGE_SIZE,MOVIE_CARD_SIZE} from '../../Config'
+import MainImage from '../LandingPage/MainImage/MainImage/MainImage'
 
 export default function MovieDetailPage(props) {
     
@@ -31,25 +32,28 @@ export default function MovieDetailPage(props) {
     return (
         <div>
             
-            this is the movie detail page
+            {Movie &&
 
-            {
-                Movie &&
-                <>
-                <p>{Movie.id}</p>
-                <p>{Movie.overview}</p>
+                <MainImage image={`${IMAGE_URL}${MAIN_IMAGE_SIZE}${Movie.backdrop_path}`} title={Movie.original_title} description={Movie.overview?Movie.overview:""} />
 
-                <p>{Movie.backdrop_path}</p>
-                <p>{Movie.belongs_to_collection}</p>
-                <p>{Movie.budget}</p>
-                <p>{Movie.genres ? Movie.genres.name :" "}</p>
-                <p>{Movie.original_language}</p>
+            }
 
-                </>
-                
-                
-
-            } 
+     <Descriptions title="Movie Detail" bordered>
+                <Descriptions.Item label="Movie_id">{Movie.id}</Descriptions.Item>
+                <Descriptions.Item label="belongs to a collection">{Movie.belongs_to_collection? Movie.belongs_to_collection:"غير متوفر"}</Descriptions.Item>
+                <Descriptions.Item label="Runtime">{Movie.runtime}</Descriptions.Item>
+                <Descriptions.Item label="Status" span={2}>
+                {Movie.status}
+                </Descriptions.Item>
+                <Descriptions.Item label="Title" span={3}>
+                            {Movie.original_title}
+                </Descriptions.Item>
+                <Descriptions.Item label="Popuarity">{Movie.popularity}</Descriptions.Item>
+                <Descriptions.Item label="Discount">{Movie.revenue}</Descriptions.Item>
+                <Descriptions.Item label="Description">
+                {Movie.overview}
+                </Descriptions.Item>
+  </Descriptions>
         </div>
     )
 }
